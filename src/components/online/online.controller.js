0c8d879b7dda57758d9f4c85ed8d19555ae6a9fa
DonSinDRom <DonSinDRom@gmail.com>
2015-01-09 06:30:32 +0400

    Added online users block

---
+++ src/components/online/online.controller.js
@@ -0,0 +1,71 @@
+'use strict';
+
+angular.module('trigger')
+  .controller('OnlineCtrl', function ($scope) {
+    $scope.date = new Date();
+    $scope.users = [
+      {
+        'name': 'ucsus'
+      },
+      {
+        'name': 'Gans'
+      },
+      {
+        'name': 'Fabeltier'
+      },
+      {
+        'name': 'Vakstal'
+      },
+      {
+        'name': 'Bamchikolachi'
+      },
+      {
+        'name': 'sinestetik'
+      },
+      {
+        'name': 'true'
+      },
+      {
+        'name': 'hitch'
+      },
+      {
+        'name': 'kpya3e'
+      },
+      {
+        'name': 'mvs059'
+      },
+      {
+        'name': 'DoZeR'
+      },
+      {
+        'name': 'Turist'
+      },
+      {
+        'name': 'overnum'
+      },
+      {
+        'name': 'bukake'
+      },
+      {
+        'name': 'andycolor'
+      },
+      {
+        'name': 'Nofets'
+      },
+      {
+        'name': 'AlexSLV'
+      },
+      {
+        'name': 'llikd'
+      },
+      {
+        'name': 'bird'
+      },
+      {
+        'name': 'S0ulTr4d3r'
+      },
+      {
+        'name': 'samlopata'
+      }
+    ]
+  });
