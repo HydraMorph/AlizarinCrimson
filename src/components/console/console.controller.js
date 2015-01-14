8412b00a041039706c66af9786b685a5a50c00d5
DonSinDRom <DonSinDRom@gmail.com>
2015-01-14 14:35:00 +0400

    Yprt

--- src/components/console/console.controller.js
+++ src/components/console/console.controller.js
@@ -1,20 +1,45 @@
 'use strict';
 
 angular.module('trigger')
-  .controller('ConsoleCtrl', function ($scope, $timeout, $mdBottomSheet) {
-    $scope.data = 'Console';
-    $scope.volume = 23;
-    $scope.changeVolume = function(volume) {
-      $scope.volume = volume;
-    };
-    $scope.openUploadBar = function($event) {
-      $scope.alert = '';
-      $mdBottomSheet.show({
-        templateUrl: 'components/upload/upload.html',
-        controller: 'UploadCtrl',
-        targetEvent: $event
-      }).then(function(clickedItem) {
-        $scope.alert = clickedItem.name + ' clicked!';
+//  .controller('ConsoleCtrl', function ($scope, $timeout, $mdDialog, $mdBottomSheet) {
+  .controller('ConsoleCtrl', function($scope, $mdDialog) {
+    $scope.showAdvanced = function(ev) {
+      $mdDialog.show({
+        controller: LoginCtrl,
+        templateUrl: 'components/login/login.html',
+        targetEvent: ev,
+      })
+      .then(function(answer) {
+        $scope.alert = 'You said the information was "' + answer + '".';
+      }, function() {
+        $scope.alert = 'You cancelled the dialog.';
       });
     };
   });
+function LoginCtrl($scope, $mdDialog) {
+  $scope.hide = function() {
+    $mdDialog.hide();
+  };
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+  $scope.answer = function(answer) {
+    $mdDialog.hide(answer);
+  };
+}
+//    $scope.data = 'Console';
+//    $scope.volume = 23;
+//    $scope.changeVolume = function(volume) {
+//      $scope.volume = volume;
+//    };
+//    $scope.openUploadBar = function($event) {
+//      $scope.alert = '';
+//      $mdBottomSheet.show({
+//        templateUrl: 'components/upload/upload.html',
+//        controller: 'UploadCtrl',
+//        targetEvent: $event
+//      }).then(function(clickedItem) {
+//        $scope.alert = clickedItem.name + ' clicked!';
+//      });
+//    };
+//  });
---
+++ src/components/login/login.controller.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('trigger')
+  .controller('LoginCtrl', function ($scope, $mdDialog) {
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+    $scope.answer = function(answer) {
+      $mdDialog.hide(answer);
+    };
+  });
