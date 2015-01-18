43ea23eeffa131774946c3932576040d35d25725
DonSinDRom <DonSinDRom@gmail.com>
2015-01-18 18:51:33 +0400

    S

--- src/components/app/app.controller.js
+++ src/components/app/app.controller.js
@@ -2,45 +2,6 @@
 
 angular.module('trigger')
   .controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $log, ngSocket, socket) {
-    var socket = ngSocket('http://trigger.fm');
-    socket.send({foo: 'bar'});
-    Client.prototype.init = function(host) {
-      console.log('init');
-     // this.socket = io.connect(host, {resource: 'socket.io'});
-      this.socket=io('http://trigger.fm');
-      var socket=this.socket;
-      var cl = this;
-
-      socket.on('welcome', function(data) {
-          $(cl).trigger('welcome', data);
-      });
-      function processLogin(data) {
-          console.log('process login', data);
-      //    var ch = 1;
-      //    client.goChannel(1, onChannel);
-      }
-
-      client = new Client();
-      console.log(client);
-      $(client).bind('welcome', function(event, data) {
-        console.log('welcome');
-        if (data) {
-          var user = 'true';
-          var pass = MD5("azaza123");
-          client.login(user, pass, processLogin);
-          console.log('user ' + user);
-      //    if (user) {
-      //      if (pass) {
-      //      } else {
-      //        client.goChannel(1, onChannel);
-      //      }
-      //    } else {
-      //      client.goChannel(1, onChannel);
-      //    }
-        }
-      });
-      client.init(location.host);
-    }
     console.log('init');
     function Client(host) {
       this.version = 2205;
