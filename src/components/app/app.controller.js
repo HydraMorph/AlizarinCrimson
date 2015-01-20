2b89c5b522ebcfb16c3f05daa28418b3f0cb1524
DonSinDRom <DonSinDRom@gmail.com>
2015-01-20 10:06:49 +0400

    S

--- src/components/app/app.controller.js
+++ src/components/app/app.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('trigger')
-  .controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+  .controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $log, socket) {
     $scope.play = true;
     $scope.togglePlay = function() {
       $scope.play = !$scope.play;
@@ -12,6 +12,21 @@ angular.module('trigger')
     $scope.toggleRight = function () {
       $mdSidenav('right').toggle();
     };
+
+    $scope.messages = [];
+    $scope.postMessage = function(message) {};
+    socket.on('connect', function () {
+      $scope.$on('socket:update', function(event, data) {
+        $scope.messages.push(data);
+      });
+
+      $scope.postMessage = function(message, callback) {
+        socket.emit('post', message, function(commitedMessage) {
+          $scope.messages.push(commitedMessage);
+          callback(commitedMessage);
+        });
+      };
+    });
   })
   .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
     $scope.close = function () {
