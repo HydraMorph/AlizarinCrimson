f63d0d3dc740c467992dc8c31286d7a2a3e976c6
DonSinDRom <DonSinDRom@gmail.com>
2015-01-21 08:13:26 +0400

    Yet another saving

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 angular.module('trigger', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMaterial', 'btford.socket-io'])
+  .value('nickName', 'true')
+  .value('password', '09e7881117ecd5e66723322ef5a6f4e0')
   .factory('socket', function ($rootScope) {
     var socket = io.connect('http://trigger.fm');
     return {
