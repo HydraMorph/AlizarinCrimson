6dd178439def1fc58c8768fae5f4f82c1729964f
DonSinDRom <DonSinDRom@gmail.com>
2015-01-21 13:02:50 +0400

    S

--- src/app/index.js
+++ src/app/index.js
@@ -3,7 +3,7 @@
 angular.module('trigger', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMaterial', 'ngSocket'])
   .value('nickName', 'true')
   .value('password', '09e7881117ecd5e66723322ef5a6f4e0')
-  .module("MyModule", ['ngSocket'])
+  .module("trigger", ['ngSocket'])
   .config(["$socketProvider", function ($socketProvider) {
     $socketProvider.setUrl("http://trigger.fm");
   }]);
