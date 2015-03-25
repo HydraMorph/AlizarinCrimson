cafdee44eaf428df8d654840b9748f8e7966ad07
DonSinDRom <DonSinDRom@gmail.com>
2015-03-25 12:24:19 +0300

    HTML -> Jade

--- src/components/navbar/navbar.controller.js
+++
@@ -1,7 +0,0 @@
-/* This is template for new modules - just ignore it */
-'use strict';
-
-angular.module('trigger')
-  .controller('NavbarCtrl', function ($scope) {
-    $scope.date = new Date();
-  });
